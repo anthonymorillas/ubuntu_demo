b35b22dbfcc431134b2755429698ba4edd8917d0
anthonymorillas <anthonymorillas@gmail.com>
2024-03-16 21:32:58 -0300

    Se limita data a 1000 filas

--- demo.py
+++ demo.py
@@ -11,7 +11,7 @@ st.title('U B U N T U')
 # @st.cache_data
 def load_data():
     # Cargar datos y especificar tipo de datos para la columna problemática
-    data = pd.read_csv('demo.csv', dtype={'column_name': str}) #nrows=100
+    data = pd.read_csv('demo.csv', nrows=1000, dtype={'column_name': str})
     rel_platos = pd.read_csv('rel_platos.csv', dtype={'column_name': str})
     platos = pd.read_csv('platos.csv', dtype={'column_name': str})
     data['createdAt'] = pd.to_datetime(data['createdAt'])
